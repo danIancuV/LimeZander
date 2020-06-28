c0f00b29234b1fd91a6f017c2726357aba90d469
iancudan <iancudan24@yahoo.com>
2020-06-28 21:48:56 +0300

    controller repo added and postman requests testing done

---
+++ Data.LimeZander.Persistence/Repositories/Interfaces/IProductRepository.cs
@@ -0,0 +1,20 @@
+﻿using Data.LimeZander.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data.LimeZander.Persistence.Repositories.Interfaces
+{
+    public interface IProductRepository
+    {
+        IEnumerable<Product> GetProducts();
+
+        Product GetProduct(int id);
+
+        void AddProduct(Product product);
+
+        void UpdateProduct(Product product);
+
+        void DeleteProduct(Product product);
+    }
+}
---
+++ Middleware.LimeZander.Api/Controllers/ProductsController.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Data.LimeZander.Model;
+using Data.LimeZander.Persistence.Repositories.Interfaces;
+
+namespace Middleware.LimeZander.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductRepository _productRepository;
+
+        public ProductsController(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        // GET: api/Products
+        [HttpGet]
+        public ActionResult<IEnumerable<Product>> GetProducts()
+        {
+            return _productRepository.GetProducts().ToList();
+        }
+
+        // GET: api/Products/5
+        [HttpGet("{id}")]
+        public ActionResult<Product> GetProduct(int id)
+        {
+            var product = _productRepository.GetProduct(id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return product;
+        }
+
+
+        // POST: api/Products
+        [HttpPost]
+        public IActionResult PostProduct(Product product)
+        {
+            _productRepository.AddProduct(product);
+
+            return Ok(product);
+        }
+
+        // PUT: api/Products/5
+        [HttpPut("{id}")]
+        public IActionResult PutProduct(int id, Product product)
+        {
+            if (id != product.Id)
+            {
+                return BadRequest();
+            }
+
+            _productRepository.UpdateProduct(product);
+
+            return NoContent();
+        }
+
+        // DELETE: api/Products/5
+        [HttpDelete("{id}")]
+        public IActionResult DeleteProduct(int id)
+        {
+            var product = _productRepository.GetProduct(id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            _productRepository.DeleteProduct(product);
+
+            return NoContent();
+        }             
+    }
+}
